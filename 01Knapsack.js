07cfb84cc06dcdb7f5757f9034ca0727ce1732c3
adrija98e <adrijaghosh.9801@gmail.com>
2022-09-30 00:37:38 +0530

    DP

--- 01Knapsack.js
+++ 01Knapsack.js
@@ -17,13 +17,35 @@
 // values[0]+values[3]+values[4] = 20+25+10 = 55
 // weights[0]+weights[3]+weights[4] = 6+10+3 = 19 <= 20
 
-const maxValue = (values, weights, k, i = 0) => {
+// Recusrsive solution
+// Time complexity = O(2^n)
+// Space complexity = O(n)
+const maxValue1 = (values, weights, k, i = 0) => {
     if(i == values.length)
         return 0;
     else if(weights[i] > k)
-        return maxValue(values, weights, k, i + 1)
+        return maxValue1(values, weights, k, i + 1)
     else
-        return Math.max(values[i] + maxValue(values, weights, k - weights[i], i + 1), maxValue(values, weights, k, i + 1));
+        return Math.max(values[i] + maxValue1(values, weights, k - weights[i], i + 1), maxValue1(values, weights, k, i + 1));
 }
 
-console.log(maxValue([37], [24], 91));
+// Dynamic programming: Memoization
+// Time complexity = O(kn)
+// Space complexity = O(kn)
+const maxValue2 = (values, weights, k, i = 0, lookup = {}) => {
+    let key = k + ":" + i;
+    if(lookup[key])
+        return lookup[key];
+    if(i == values.length)
+        return 0;
+    else if(weights[i] > k) {
+        lookup[key] = maxValue2(values, weights, k, i + 1);
+        return lookup[key];
+    }
+    else {
+        lookup[key] = Math.max(values[i] + maxValue2(values, weights, k - weights[i], i + 1), maxValue2(values, weights, k, i + 1));
+        return lookup[key];
+    }
+}
+
+console.log(maxValue2([37], [24], 91));
